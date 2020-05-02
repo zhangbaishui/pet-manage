770aa6a8398a4b0d72855e3522339db7f5a7ab47
张泉 <5308591+zhangbaishui@user.noreply.gitee.com>
2020-05-02 15:55:22 +0800

    后端接口走网关，使用cors解决跨域问题

--- code-src/src/main/java/com/zq/controller/UserController.java
+++ code-src/src/main/java/com/zq/controller/UserController.java
@@ -3,8 +3,9 @@ package com.zq.controller;
 import com.zq.pojo.User;
 import com.zq.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,18 +13,54 @@ import java.util.List;
  * Author: Quan.Zhang
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/test")
-    public  String  test(){
-        return  "傻逼一号卢本伟准备就绪";
+
+    /*测试专用*/
+    @RequestMapping("/user")
+    public List<User> user() {
+        List<User> users = userService.selectAllUser();
+        return users;
     }
 
-    @RequestMapping("/user")
-    public List<User> user(){
-        List<User> users = userService.selectAllUser();
-        return users ;
+    /**/
+    /*校验用户名是否存在*/
+    @RequestMapping("/nameIsEixt")
+    @ResponseBody
+    public Boolean nameIsEixt(@RequestParam(value = "name")String name) {
+        Boolean res = userService.nameIsEixt(name);
+        return res;
     }
+
+    /*添加用户*/
+    @PostMapping("/add")
+    public ResponseEntity<Void> addUser(User user) {
+        userService.addUser(user);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    /*修改用户*/
+    @PostMapping("/update")
+    public boolean updateUser(User user) {
+        Boolean result = userService.upadateUser(user);
+        return result;
+    }
+
+    /*删除用户*/
+    @PostMapping("/delete")
+    public boolean deleteUser(User user) {
+        Boolean result = userService.deleteUser(user);
+        return result;
+    }
+
+    /*根据user名称查询用户*/
+    @PostMapping("/queryByName")
+    public User selectUserByName(String name) {
+        User user = userService.selectUserByName(name);
+        return user;
+    }
+
 }
--- code-src/src/main/java/com/zq/service/UserService.java
+++ code-src/src/main/java/com/zq/service/UserService.java
@@ -36,5 +36,54 @@ public class UserService {
         return users;
     }
 
+    public void addUser(User user) {
+        if (user.getName() != null && user.getPass() != null) {
+            int insert = userMapper.insert(user);
+        }
+
+    }
+
+    public Boolean deleteUser(User user) {
+        int delete = userMapper.delete(user);
+        if (delete > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User selectUserByName(String name) {
+        User user = new User();
+        user.setName(name);
+        User select = userMapper.selectOne(user);
+        if (select.getId() == null) {
+            return null;
+        } else {
+            return select;
+        }
+
+    }
+
+    public Boolean upadateUser(User user) {
+        int i = userMapper.updateByPrimaryKey(user);
+        if (i > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean nameIsEixt(String name) {
+        System.out.println(name);
+        User user = new User();
+        user.setName(name);
+        List<User> select = userMapper.select(user);
+        if (select.size()>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     ;
 }
